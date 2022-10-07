74e4f37fd1481f6b8d71c372152d509dd9473e51
Cheon Jiyeon / Leezy <secleezy@gmail.com>
2022-10-07 13:56:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cbnugra/MainActivity.java
@@ -0,0 +1,28 @@
+    package com.example.cbnugra;
+
+    import androidx.appcompat.app.AppCompatActivity;
+
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //화면전환
+                Intent intent = new Intent(MainActivity.this, join.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
