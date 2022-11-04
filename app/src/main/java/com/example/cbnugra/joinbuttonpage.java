a62033777b586b6e345efdd6b44ebc4f05308591
kgu011016 <kgu011016@gamil.com>
2022-11-04 14:18:40 +0900

    11-04 아이디값 정렬

--- app/src/main/java/com/example/cbnugra/joinbuttonpage.java
+++ app/src/main/java/com/example/cbnugra/joinbuttonpage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -12,7 +14,9 @@ import android.os.Bundle;
 import org.w3c.dom.Text;
 
 public class joinbuttonpage extends AppCompatActivity {
-    TextView textView;
+    TextView textView; //스피너(textView)
+    Button id_checked; //중복확인버튼(id_checked)
+    EditText joinid, joinpw, joinname, birth, phon_num;
 
     //스피너 배열 값
     String[] items={"선택안함","여자","남자" };
@@ -20,6 +24,18 @@ public class joinbuttonpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joinbuttonpage);
+
+        //입력 값 아이디(아이디, 비밀번호, 전화번호, 생일, 이름)
+        joinid=findViewById(R.id.joinid);
+        joinpw=findViewById(R.id.joinpw);
+        joinname=findViewById(R.id.name);
+        birth=findViewById(R.id.birth);
+        phon_num=findViewById(R.id.call_num);
+
+        //아이디 중복확인
+        id_checked=findViewById(R.id.joinid_checked);
+
+
 
         //스피너(성별선택 코드)
         Spinner spinner=findViewById(R.id.change_sex);
