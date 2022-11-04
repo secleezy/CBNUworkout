10411b3bb7731163b55b8b7c60f596ae40ec1f31
Cheon Jiyeon / Leezy <secleezy@gmail.com>
2022-11-04 16:45:34 +0900

    1104 서버연동 성공 이제 폼에 맞춰서 데이터베이스 추가해야함

--- app/src/main/java/com/example/cbnugra/joinbuttonpage.java
+++ app/src/main/java/com/example/cbnugra/joinbuttonpage.java
@@ -10,6 +10,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,7 +22,11 @@ public class joinbuttonpage extends AppCompatActivity {
 
     //회원가입 데이터베이스 연결코드
     EditText joinid; //1104천지연
+    EditText joinpw;
     Button go_join;
+
+    String ID;
+    String PW;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -40,7 +45,10 @@ public class joinbuttonpage extends AppCompatActivity {
 
         //회원가입 데이터베이스 연결코드
         joinid = (EditText) findViewById(R.id.joinid);
+        joinpw = (EditText) findViewById(R.id.joinpw);
         go_join = (Button) findViewById(R.id.go_join);
+
+
 
         //스피너(성별선택 코드)
         Spinner spinner=findViewById(R.id.change_sex);
@@ -64,10 +72,35 @@ public class joinbuttonpage extends AppCompatActivity {
         //버튼 클릭시
         go_join.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                databaseReference.child("message").push().setValue("2");
+                ID = joinid.getText().toString();
+                PW = joinpw.getText().toString();
+
+                if(ID.isEmpty() || PW.isEmpty())
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "짧은 토스트 메시지입니다.",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else
+                    adduser(ID, PW);
+
             }
         });
     }
 
+    //값을 파이어베이스 Realtime database로 넘기는 함수
+    public void adduser(String ID, String PW) {
+
+        //여기에서 직접 변수를 만들어서 값을 직접 넣는것도 가능합니다.
+        // ex) 갓 태어난 동물만 입력해서 int age=1; 등을 넣는 경우
+
+        //animal.java에서 선언했던 함수.
+        userdb userdb = new userdb(ID,PW);
+
+        //child는 해당 키 위치로 이동하는 함수입니다.
+        //키가 없는데 "zoo"와 name같이 값을 지정한 경우 자동으로 생성합니다.
+        databaseReference.child("user").child(ID).setValue(userdb);
+
+
+    }
 
 }
