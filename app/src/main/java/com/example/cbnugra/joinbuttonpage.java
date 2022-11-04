0afa194d9ed739af265857eeefce85e297578526
kgu011016 <kgu011016@gamil.com>
2022-11-04 13:54:48 +0900

    2022-11-04 회원가입 페이지 스피너 자바 코드 추가

--- app/src/main/java/com/example/cbnugra/joinbuttonpage.java
+++ app/src/main/java/com/example/cbnugra/joinbuttonpage.java
@@ -1,14 +1,44 @@
 package com.example.cbnugra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
-public class joinbuttonpage extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class joinbuttonpage extends AppCompatActivity {
+    TextView textView;
+
+    //스피너 배열 값
+    String[] items={"선택안함","여자","남자" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joinbuttonpage);
+
+        //스피너(성별선택 코드)
+        Spinner spinner=findViewById(R.id.change_sex);
+        TextView textView=findViewById(R.id.show_sex);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, items
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int positions, long l) {
+                textView.setText(items[positions]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                textView.setText("");
+            }
+        });
     }
 }
