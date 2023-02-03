301353ba23d5d3ef7b7ee0939584ee11cadc005b
Cheon Jiyeon / Leezy <secleezy@gmail.com>
2023-02-03 18:43:59 +0900

    20230203 운동 추가가능!

--- app/src/main/java/com/example/cbnugra/mainhome_home.java
+++ app/src/main/java/com/example/cbnugra/mainhome_home.java
@@ -5,17 +5,51 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import android.widget.Toast;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class mainhome_home extends Fragment {
     private View view;
     private TextView title;
     private String name;
+    private Button workoutbt;
 
+    EditText year;
+    EditText month;
+    EditText day;
+    EditText workoutname;
+    EditText kcal;
+
+    String time_log;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+
+    private String getTime() { //현재 시간 구하는 함수, getTime()으로 사용
+        TimeZone tz;
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREAN);
+        tz = TimeZone.getTimeZone("Asia/Seoul");
+        dateFormat.setTimeZone(tz);
+        String getTime = dateFormat.format(date);
+
+        return getTime;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,8 +61,16 @@ public class mainhome_home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         view = inflater.inflate(R.layout.fragment_mainhome_home, container, false);
         title = view.findViewById(R.id.title);
+        workoutbt = view.findViewById(R.id.workoutbt);
+
+        year = view.findViewById(R.id.year);
+        month = view.findViewById(R.id.month);
+        day = view.findViewById(R.id.day);
+        workoutname = view.findViewById(R.id.workoutname);
+        kcal = view.findViewById(R.id.kcal);
 
         if (getArguments() != null)
         {
@@ -37,6 +79,41 @@ public class mainhome_home extends Fragment {
             //name이 id임. name이 id임.  name이 id임.  name이 id임.
         }
 
+        //운동기록 저장 클릭시
+        workoutbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String year_text = year.getText().toString();
+                String month_text = month.getText().toString();
+                String day_text = day.getText().toString();
+                String workoutname_text = workoutname.getText().toString();
+                String kcal_text = kcal.getText().toString();
+
+                if(year_text.isEmpty()||month_text.isEmpty()||day_text.isEmpty()||workoutname_text.isEmpty()||kcal_text.isEmpty())
+                {
+                    Toast toast = Toast.makeText(getActivity(), "모든 항목을 입력 해 주세요.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    Toast toast = Toast.makeText(getActivity(), "성공적으로 저장되었습니다..", Toast.LENGTH_SHORT);
+                    toast.show();
+                    time_log=getTime();
+                    addworkout(name, time_log, year_text, month_text, day_text, workoutname_text, kcal_text);
+                }
+            }
+        });
+
         return view;
+    }
+
+    //값을 파이어베이스 Realtime database로 넘기는 함수
+    public void addworkout(String ID, String time_log, String Year, String Month, String Day, String workoutname, String kcal) {
+
+
+        userworkout userworkout = new userworkout(ID, time_log, Year, Month, Day, workoutname, kcal);
+        databaseReference.child("workout").child(ID).child(time_log).setValue(userworkout);
+
+
     }
 }
--- app/src/main/java/com/example/cbnugra/userworkout.java
+++ app/src/main/java/com/example/cbnugra/userworkout.java
@@ -5,11 +5,12 @@ package com.example.cbnugra;
 public class userworkout {
 
     private String ID; // 아이디
-    private String year;
-    private String month;
-    private String day;
-    private String workoutname;
-    private String kcal;
+    private String time_log; //현재시각
+    private String Year; //날짜
+    private String Month;
+    private String Day;
+    private String workoutname; //운동이름
+    private String kcal; //칼로리
 
     public userworkout(){} // 생성자 메서드
 
@@ -22,10 +23,57 @@ public class userworkout {
         this.ID = ID;
     }
 
+    public String gettime_log() {
+        return time_log;
+    }
+    public void settime_log(String name) {
+        this.time_log = time_log;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+    public void setYear(String name) {
+        this.Year = Year;
+    }
+
+    public String getMonth() {
+        return Month;
+    }
+    public void setMonth(String name) {
+        this.Month = Year;
+    }
+
+    public String getDay() {
+        return Day;
+    }
+    public void setDay(String name) {
+        this.Day = Day;
+    }
+
+    public String getworkoutname() {
+        return workoutname;
+    }
+    public void setworkoutname(String name) {
+        this.workoutname = workoutname;
+    }
+
+    public String getkcal() {
+        return kcal;
+    }
+    public void setkcal(String name) {
+        this.Day = kcal;
+    }
+
 
     //값을 추가할때 쓰는 함수, MainActivity에서 add함수에서 사용할 것임.
-    public userworkout(String ID){
+    public userworkout(String ID, String time_log, String Year,String Month,String Day,String workoutname,String kcal){
         this.ID = ID;
-
+        this.time_log=time_log;
+        this.Year = Year;
+        this.Month=Month;
+        this.Day=Day;
+        this.workoutname=workoutname;
+        this.kcal=kcal;
     }
 }
