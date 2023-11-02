a6fc53dcd683938327371065923187a5c98686b5
한지성 <wltjd1925@naver.com>
2023-11-02 17:37:35 +0900

    운동기록 리스트뷰

---
+++ app/src/main/java/com/example/cbnugra/CustomListView.java
@@ -0,0 +1,61 @@
+package com.example.cbnugra;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomListView  extends BaseAdapter {
+    LayoutInflater layoutInflater = null;
+    private ArrayList<ListData> listViewData = null;
+    private int count = 0;
+
+    public CustomListView(ArrayList<ListData> listData)
+    {
+        listViewData = listData;
+        count = listViewData.size();
+    }
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null)
+        {
+            final Context context = viewGroup.getContext();
+            if (layoutInflater == null)
+            {
+                layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+            view = layoutInflater.inflate(R.layout.custom_listview, viewGroup, false);
+        }
+
+
+        TextView title = view.findViewById(R.id.title);
+        TextView body_1 = view.findViewById(R.id.body_1);
+        TextView body_2 = view.findViewById(R.id.body_2);
+
+        title.setText(listViewData.get(i).title);
+        body_1.setText(listViewData.get(i).body_1);
+        body_2.setText(listViewData.get(i).body_2);
+        return view;
+
+    }
+}
+
--- app/src/main/java/com/example/cbnugra/record.java
+++ app/src/main/java/com/example/cbnugra/record.java
@@ -2,13 +2,16 @@ package com.example.cbnugra;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -31,13 +35,10 @@ import java.util.TimeZone;
 public class record extends AppCompatActivity {
 
     ListView listView1;
-    ArrayAdapter<String> adapter;
-    ArrayList<String> listItem;
-    TextView YYMMDD,txt1,txt2,txt3,txt4,txt5,txt6;
-    EditText exercisename;
-    EditText editText1;
-    Button addbutton;
-    String itemname;
+    TextView YYMMDD;
+
+    Button addbutton, txt1, txt2, txt3, txt4, txt5, txt6;
+
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -56,8 +57,6 @@ public class record extends AppCompatActivity {
         String yearMonDay = intent.getStringExtra("ymd");
         YYMMDD = findViewById(R.id.YYMMDD);
         YYMMDD.setText(yearMonDay);
-        exercisename = findViewById(R.id.exercisename);
-        editText1 = findViewById(R.id.editText1);
         addbutton = findViewById(R.id.addbutton);
         txt1 = findViewById(R.id.txt1);
         txt2 = findViewById(R.id.txt2);
@@ -65,7 +64,7 @@ public class record extends AppCompatActivity {
         txt4 = findViewById(R.id.txt4);
         txt5 = findViewById(R.id.txt5);
         txt6 = findViewById(R.id.txt6);
-
+        listView1 = findViewById(R.id.list_view_exerciseroutine);
         foodRecordReference.orderByKey().addListenerForSingleValueEvent(new ValueEventListener() {
 
 
@@ -73,8 +72,9 @@ public class record extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot userSnapshot : snapshot.getChildren()) {
                     if (userSnapshot.getKey().equals(UserID)) {
-                        for(DataSnapshot daySnapshot : userSnapshot.getChildren()){
-                            if(daySnapshot.getKey().equals(yearMonDay)) {
+                        for (DataSnapshot daySnapshot : userSnapshot.getChildren()) {
+
+                            if (daySnapshot.getKey().equals(yearMonDay)) {
                                 String str1, str2, str3, str4, str5, str6;
                                 for (DataSnapshot foodSnapshot : daySnapshot.getChildren()) {
                                     if (foodSnapshot.getKey().equals("아침")) {
@@ -82,30 +82,30 @@ public class record extends AppCompatActivity {
                                         txt1.setText(str1);
                                     }
 
-                                if (foodSnapshot.getKey().equals("오전간식")) {
-                                    str2 = foodSnapshot.getValue(String.class);
-                                    txt2.setText(str2);
-                                }
-                                if (foodSnapshot.getKey().equals("아침")) {
-                                    str3 = foodSnapshot.getValue(String.class);
-                                    txt3.setText(str3);
-                                }
-                                if (foodSnapshot.getKey().equals("오후간식")) {
-                                    str4 = foodSnapshot.getValue(String.class);
-                                    txt4.setText(str4);
-                                }
-                                if (foodSnapshot.getKey().equals("저녁")) {
-                                    str5 = foodSnapshot.getValue(String.class);
-                                    txt5.setText(str5);
-                                }
-                                if (foodSnapshot.getKey().equals("야식")) {
-                                    str6 = foodSnapshot.getValue(String.class);
-                                    txt6.setText(str6);
+                                    if (foodSnapshot.getKey().equals("오전간식")) {
+                                        str2 = foodSnapshot.getValue(String.class);
+                                        txt2.setText(str2);
+                                    }
+                                    if (foodSnapshot.getKey().equals("아침")) {
+                                        str3 = foodSnapshot.getValue(String.class);
+                                        txt3.setText(str3);
+                                    }
+                                    if (foodSnapshot.getKey().equals("오후간식")) {
+                                        str4 = foodSnapshot.getValue(String.class);
+                                        txt4.setText(str4);
+                                    }
+                                    if (foodSnapshot.getKey().equals("저녁")) {
+                                        str5 = foodSnapshot.getValue(String.class);
+                                        txt5.setText(str5);
+                                    }
+                                    if (foodSnapshot.getKey().equals("야식")) {
+                                        str6 = foodSnapshot.getValue(String.class);
+                                        txt6.setText(str6);
+                                    }
                                 }
                             }
-                            }
                         }
-                        }
+                    }
                 }
             }
 
@@ -114,28 +114,35 @@ public class record extends AppCompatActivity {
 
             }
         });
-        List<String> userworklist = new ArrayList<>();
+
+        ArrayList<ListData> listViewData = new ArrayList<>();
         workRecordReference.orderByKey().addListenerForSingleValueEvent(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot userSnapshot : snapshot.getChildren()) {
                     if (userSnapshot.getKey().equals(UserID)) {
-                        for(DataSnapshot daySnapshot : userSnapshot.getChildren()){
-                            if(daySnapshot.getKey().equals(yearMonDay)) {
+                        for (DataSnapshot timeSnapshot : userSnapshot.getChildren()) {
+                            for (DataSnapshot dataSnapshot : timeSnapshot.getChildren()) {
 
-                                    String workoutname;
-                                    String kcal;
-                                    workoutname = daySnapshot.child("workoutname").getValue(String.class);
-                                    kcal = daySnapshot.child("kcal").getValue(String.class);
-                                    if (workoutname != null && kcal != null){
-                                    userworklist.add(workoutname +"-"+ kcal);}
+                                if (dataSnapshot.getValue().equals(yearMonDay)) {
+                                    ListData listData = new ListData();
 
+                                    listData.title = timeSnapshot.child("workoutname").getValue(String.class);
+                                    listData.body_1 = timeSnapshot.child("kcal").getValue(String.class);
+                                    listData.body_2 = "kcal";
+
+                                    listViewData.add(listData);
+                                    //"work",timeSnapshot.child("workoutname").getValue(String.class)
+                                    //"kcal",timeSnapshot.child("kcal").getValue(String.class)
+                                }
                             }
+
                         }
                     }
                 }
-
+                ListAdapter oAdapter = new CustomListView(listViewData);
+                listView1.setAdapter(oAdapter);
             }
 
             @Override
@@ -143,64 +150,26 @@ public class record extends AppCompatActivity {
 
             }
         });
-        listItem = new ArrayList<String>();
-        for(int i = 0; i < userworklist.size(); i++){
-            listItem.add(userworklist.get(i));
-            adapter.notifyDataSetChanged();
-        }
 
-
-        // 아이템 추가한다. [DB작업필요]!!!!!!
-        addbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //
-                //addworkout(ID, getTime(), year_text, month_text, day_text, exercisename.getText().toString(), editText1.getText().toString());
-                //
-
-                itemname=exercisename.getText().toString() + "  -  " + exercisename.getText().toString();
-                listItem.add(itemname);
-                adapter.notifyDataSetChanged(); // 변경되었음을 어답터에 알려준다.
-                exercisename.setText("");
-                editText1.setText("");
-
-
-
-
-            }
-        });
-
-
-        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, listItem);
-        listView1 = findViewById(R.id.list_view_exerciseroutine);
-        listView1.setAdapter(adapter);
-
-        // 각 아이템 클릭시 해당 아이템 삭제한다. [DB작업필요]!!!!!!
         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            // 콜백매개변수는 순서대로 어댑터뷰, 해당 아이템의 뷰, 클릭한 순번, 항목의 아이디
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Toast.makeText(getApplicationContext(), listItem.get(i).toString() + " 삭제되었습니다.", Toast.LENGTH_SHORT).show();
-
-                listItem.remove(i);
-                adapter.notifyDataSetChanged();
             }
         });
-
-        //리스트 뷰 높이 지정
-        int totalHieght = 0;
-        for (int i = 0; i < adapter.getCount(); i++) {
-            View listItem = adapter.getView(i, null, listView1);
-            listItem.measure(0, 0);
-            totalHieght += listItem.getMeasuredHeight();
-        }
-        ViewGroup.LayoutParams params = listView1.getLayoutParams();
-        params.height = totalHieght + (listView1.getDividerHeight() * (adapter.getCount() - 1));
-        listView1.setLayoutParams(params);
-        listView1.setAdapter(adapter);
+txt1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {/*
+        String foodname = (String) txt1.getText();
+        Intent intent1 = new Intent(getApplicationContext(), food_info.class);
+        if(foodname != "아침식사"){
+        intent1.putExtra("ymd", yearMonDay);
+        intent1.putExtra("food", foodname);
+            startActivity(intent1);
+    }*/
     }
+});
+        }
 
     //값을 파이어베이스 Realtime database로 넘기는 함수
     public void addworkout(String ID, String time_log, String Year, String Month, String Day, String workoutname, String kcal) {
@@ -211,4 +180,5 @@ public class record extends AppCompatActivity {
 
 
     }
+
 }
